dcb5317c0fad90abac07d7f4902f8e852df9cb5f
BlackCab <littlebig_planet@ukr.net>
2016-02-04 16:03:15 +0200

    доделал немного

--- js/calc.js
+++ js/calc.js
@@ -7,3 +7,19 @@ function hidePopUp(id) {
                     popUp = document.getElementById(id);
                     popUp.style.display = "none";
                 }
+				
+function passData(from_id, to_id) {
+                   from = document.getElementById(from_id);
+                   to = document.getElementById(to_id);
+                   if (from.value !="") {
+                   to.value = from.value;
+                       } else {
+				    to.value = from.placeholder;   
+				    } 			   
+                }
+
+function passRadioBtnStatus(from_radio_id, to_radio_id) {
+                   from_radiobtn = document.getElementById(from_radio_id);
+                   to_radiobtn = document.getElementById(to_radio_id);
+                   to_radiobtn.checked = from_radiobtn.checked;				   
+}
