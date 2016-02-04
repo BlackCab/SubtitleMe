57015daea6e396a6499e01723863fea7be6daf1d
BlackCab <littlebig_planet@ukr.net>
2016-02-04 12:44:15 +0200

    добавил жс

---
+++ js/calc.js
@@ -0,0 +1,9 @@
+function showPopUp(id) {
+		    popUp = document.getElementById(id);
+		    popUp.style.display = "block";
+		}
+		
+function hidePopUp(id) {
+                    popUp = document.getElementById(id);
+                    popUp.style.display = "none";
+                }
